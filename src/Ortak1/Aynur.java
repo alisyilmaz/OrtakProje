1e396f29a0795a670f8e6da67056bc02bb043d76
aynur <ay.oktay79@gmail.com>
2023-01-30 19:48:12 +0100

    ucuncu task

--- src/Ortak1/Aynur.java
+++ src/Ortak1/Aynur.java
@@ -4,6 +4,6 @@ public class Aynur {
     public static void main(String[] args) {
         System.out.println("ilk deneme");
         System.out.println("ikinci deneme");
-
+        System.out.println("ucuncu deneme");
     }
 }
