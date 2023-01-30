a03cafdef3127504a8fe3d600ddb0d332517f692
aynur <ay.oktay79@gmail.com>
2023-01-30 19:42:18 +0100

    ikinci task

--- src/Ortak1/Aynur.java
+++ src/Ortak1/Aynur.java
@@ -3,5 +3,7 @@ package Ortak1;
 public class Aynur {
     public static void main(String[] args) {
         System.out.println("ilk deneme");
+        System.out.println("ikinci deneme");
+
     }
 }
